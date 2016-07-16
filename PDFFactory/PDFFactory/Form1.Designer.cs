233baa12e9af671302fb514d2c722f699f92c620
Dave Carson <dave@onthefencedevelopment.com>
2016-07-16 17:27:20 +0100

    Initial commit of basic join functionality

---
+++ PDFFactory/PDFFactory/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PDFFactory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFile1 = new System.Windows.Forms.TextBox();
+            this.txtFile2 = new System.Windows.Forms.TextBox();
+            this.btnBrowse1 = new System.Windows.Forms.Button();
+            this.btnBrowse2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(220, 118);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.TabIndex = 0;
+            this.btnJoin.Text = "Join";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(301, 118);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Close";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File 2:";
+            // 
+            // txtFile1
+            // 
+            this.txtFile1.Location = new System.Drawing.Point(54, 29);
+            this.txtFile1.Name = "txtFile1";
+            this.txtFile1.ReadOnly = true;
+            this.txtFile1.Size = new System.Drawing.Size(241, 20);
+            this.txtFile1.TabIndex = 4;
+            // 
+            // txtFile2
+            // 
+            this.txtFile2.Location = new System.Drawing.Point(54, 60);
+            this.txtFile2.Name = "txtFile2";
+            this.txtFile2.ReadOnly = true;
+            this.txtFile2.Size = new System.Drawing.Size(241, 20);
+            this.txtFile2.TabIndex = 5;
+            // 
+            // btnBrowse1
+            // 
+            this.btnBrowse1.Location = new System.Drawing.Point(301, 27);
+            this.btnBrowse1.Name = "btnBrowse1";
+            this.btnBrowse1.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse1.TabIndex = 6;
+            this.btnBrowse1.Text = "Browse";
+            this.btnBrowse1.UseVisualStyleBackColor = true;
+            this.btnBrowse1.Click += new System.EventHandler(this.btnBrowse1_Click);
+            // 
+            // btnBrowse2
+            // 
+            this.btnBrowse2.Location = new System.Drawing.Point(301, 58);
+            this.btnBrowse2.Name = "btnBrowse2";
+            this.btnBrowse2.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse2.TabIndex = 7;
+            this.btnBrowse2.Text = "Browse";
+            this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 118);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 150);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBrowse2);
+            this.Controls.Add(this.btnBrowse1);
+            this.Controls.Add(this.txtFile2);
+            this.Controls.Add(this.txtFile1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnJoin);
+            this.Name = "Form1";
+            this.Text = "PDF Factory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFile1;
+        private System.Windows.Forms.TextBox txtFile2;
+        private System.Windows.Forms.Button btnBrowse1;
+        private System.Windows.Forms.Button btnBrowse2;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ PDFFactory/PDFFactory/Form1.cs
@@ -0,0 +1,126 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Windows.Forms;
+
+namespace PDFFactory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse1_Click(object sender, EventArgs e)
+        {
+            ShowFileDialog(txtFile1);
+            ValidateSelectedPath(txtFile1);
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            ShowFileDialog(txtFile2);
+            ValidateSelectedPath(txtFile2);
+        }
+
+        private void ShowFileDialog(TextBox targetTextBox)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "PDF Files (*.pdf) | *.pdf",
+                Multiselect = false
+            };
+
+            openFileDialog.ShowDialog();
+
+            if (!string.IsNullOrEmpty(openFileDialog.FileName))
+            {
+                targetTextBox.Text = openFileDialog.FileName;
+            }
+
+            btnJoin.Enabled = CanJoin;
+        }
+
+        private bool CanJoin
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(txtFile1.Text) && !string.IsNullOrEmpty(txtFile2.Text);
+            }
+        }
+
+        private void ValidateSelectedPath(TextBox targetTextBox)
+        {
+            if (txtFile1.Text == txtFile2.Text)
+            {
+                DialogResult result = MessageBox.Show(this, "You have selected the same path for each file - is this correct?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.No)
+                {
+                    targetTextBox.Text = string.Empty;
+                    btnJoin.Enabled = CanJoin;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtFile1.Text = string.Empty;
+            txtFile2.Text = string.Empty;
+            btnJoin.Enabled = CanJoin;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] bytes = null;
+                string[] array = new string[]
+                {
+                    txtFile1.Text,
+                    txtFile2.Text
+                };
+
+                // TODO: Prompt for filename
+
+                using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+                {
+                    using (Document document = new Document())
+                    {
+                        using (PdfCopy pdfCopy = new PdfCopy(document, memoryStream))
+                        {
+                            document.Open();
+                            for (int i = 0; i < 2; i++)
+                            {
+                                PdfReader pdfReader = new PdfReader(array[i]);
+                                int numberOfPages = pdfReader.NumberOfPages;
+                                int j = 0;
+                                while (j < numberOfPages)
+                                {
+                                    pdfCopy.AddPage(pdfCopy.GetImportedPage(pdfReader, ++j));
+                                }
+                            }
+                        }
+                    }
+                    bytes = memoryStream.ToArray();
+                }
+                string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                System.IO.File.WriteAllBytes(System.IO.Path.Combine(folderPath, "MergedDocument.pdf"), bytes);
+                txtFile1.Text = string.Empty;
+                txtFile2.Text = string.Empty;
+                btnJoin.Enabled = CanJoin;
+
+                MessageBox.Show("File Joined Successfully");
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+        }
+    }
+}
